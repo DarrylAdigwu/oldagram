6f6d8331634573567865068eb684f968c9f4f663
DarrylAdigwu <115912161+DarrylAdigwu@users.noreply.github.com>
2026-06-26 21:46:55 -0700

    finished project

--- src/index.js
+++ src/index.js
@@ -1,30 +1,43 @@
-const posts = [
-    {
-        name: "Vincent van Gogh",
-        username: "vincey1853",
-        location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
-        comment: "just took a few mushrooms lol",
-        likes: 21
-    },
-    {
-        name: "Gustave Courbet",
-        username: "gus1819",
-        location: "Ornans, France",
-        avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
-        comment: "i'm feelin a bit stressed tbh",
-        likes: 4
-    },
-        {
-        name: "Joseph Ducreux",
-        username: "jd1735",
-        location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
-        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
-    }
-]
+import { posts } from "./post";
 
+const mainPage = document.getElementById("user-feed");
+
+let allPosts = '';
+
+function getPosts() {
+  for(let i = 0; i < posts.length; i++) {
+    allPosts += `
+      <section class="posts">
+        <div class="post-header">
+          <img class="post-avatar" src=${posts[i].avatar} alt=${posts[i].name}>
+          <div class="post-info">
+            <p class="post-username">${posts[i].name}</p>
+            <p class="post-user-location">${posts[i].location}</p>
+          </div>
+        </div>
+        <div class="post-content">
+          <img class="content-image" src=${posts[i].post}>
+        </div>
+        <div class="post-interactions">
+          <div class="interaction-icons">
+            <img class="interact-icon-like" src="src/images/icon-heart.png" >
+            <img class="interact-icon-comment" src="src/images/icon-comment.png" >
+            <img class="interact-icon-share" src="src/images/icon-dm.png" >
+          </div>
+          <div class="likes-count">${posts[i].likes} likes</div>
+          <div class="comment-section">
+            <span class="commenter-name">${posts[i].username}<span>
+            <span class="commenter-comment">${posts[i].comment}<span>
+          </div>
+        </div>
+      </section>
+    `
+  }
+  return allPosts;
+}
+
+function render() {
+  mainPage.innerHTML = getPosts();
+}
+
+render();
---
+++ src/post.js
@@ -0,0 +1,30 @@
+export const posts = [
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "src/images/avatar-vangogh.jpg",
+        post: "src/images/post-vangogh.jpg",
+        comment: "just took a few mushrooms lol",
+        likes: 21
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "src/images/avatar-courbet.jpg",
+        post: "src/images/post-courbet.jpg",
+        comment: "i'm feelin a bit stressed tbh",
+        likes: 4
+    },
+        {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "src/images/avatar-ducreux.jpg",
+        post: "src/images/post-ducreux.jpg",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152
+    }
+]
+
